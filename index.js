62e907e99e8739a5dfa89cc5ea1707e9c733c8c1
Ajayi Toluwani <122868845+Ajayi98@users.noreply.github.com>
2023-07-31 13:15:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import { generateTextAndImage } from "./utils.js"
+
+let name = "Ajayi Toluwani"
+
+
+let favoriteActivity = "coding"
+
+
+let favoritePlace = "Altschool Africa"
+
+
+let temperature = 0.9
+
+
+// Optional: delete "avatar.jpg" and add a photo of yourself
+// (remember to use "avatar.jpg" as the name of your photo)
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
